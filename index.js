04608872039d5f3d623beba466c746be9ab018b9
Richard Kroon <kroon.richard@gmail.com>
2018-04-25 00:20:04 +0200

    Een beetje test code

---
+++ index.js
@@ -0,0 +1,24 @@
+const config = {
+    host: "localhost",
+    port: 1883,
+    mqttId: "some-unique-id"
+}
+
+const client = require('mqtt').connect(config)
+
+client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe('/some/topic')
+})
+
+client.on('message', (topic, messageBuf) => {
+    console.log('Topic: ' + topic)
+    console.log('Message: \n' + messageBuf.toString())
+})
+
+const repeatPublishSomething = () => {
+    client.publish('/some/topic', 'yoho yoho')
+    setTimeout(() => repeatPublishSomething(), 5000)
+}
+
+repeatPublishSomething()
